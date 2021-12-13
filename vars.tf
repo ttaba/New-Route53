5b6a007a63b87cf837fd4a51bd83b85a18ec9c3f
ttaba <77976876+ttaba@users.noreply.github.com>
2021-12-13 11:07:54 -0500

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,42 @@
+variable "name" {
+  description = "Rule Name"
+  type        = string
+}
+
+variable "associated_vpcs" {
+  description = "List of VPC IDs to associate rule to"
+  type        = list(string)
+}
+
+variable "forward_domain" {
+  description = "Domain name to forward requests for"
+  type        = string
+}
+
+variable "forward_ips" {
+  description = "List of IPs to forward DNS requests to"
+  type        = list(string)
+}
+
+variable "dns_port" {
+  type        = number
+  description = "DNS port to forward DNS requests to"
+  default     = 53
+}
+
+variable "resolver_endpoint" {
+  description = "Resolver Endpoint ID"
+  type        = string
+}
+
+variable "resource_share_accounts" {
+  default     = []
+  description = "List of account IDs to share this resolver rule with"
+  type        = list(string)
+}
+
+variable "tags" {
+  default     = {}
+  description = "Map of tags to apply to supported resources"
+  type        = map(string)
+}
